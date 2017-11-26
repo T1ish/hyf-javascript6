a35404818452ff4a01eddd4a7b20d4d4571826eb
T1ish <hisuiismine@yahoo.com>
2017-11-26 09:20:10 +0100

    Adding comments to previous step

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+// Here we get searchBtn from the html page
 const searchButton = document.querySelector("#searchBtn");
 
-
+//Here we uses the variable searchButton to listen to if there happens a click even and then output the console mnessage "You clicked me!"
 searchButton.addEventListener('click', function(){
 	console.log("You clicked me!");
 });
